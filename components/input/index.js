ea00634878a5d9bf51bfd704b4ee7bcf0e3beed3
Chuks Festus <cutogu@gmail.com>
2020-11-14 18:46:59 +0100

    text proptypes

--- components/input/index.js
+++ components/input/index.js
@@ -6,56 +6,61 @@ import CustomText from "../../atoms/text";
 import { colors } from "../../atoms/colors";
 
 const Input = (props) => {
-  const [focus, setFocus] = React.useState(false);
+	const [focus, setFocus] = React.useState(false);
 
-  return (
-    <View style={{ width: "100%" }}>
-      <CustomText type="medium" style={{ textTransform: "capitalize" }}>
-        {props.label}
-      </CustomText>
-      <TextInput
-        placeholder={props.placeholder}
-        secureTextEntry={props.secureTextEntry}
-        value={props.value}
-        onChangeText={props.onChangeText}
-        onFocus={() => setFocus(true)}
-        onBlur={() => setFocus(false)}
-        style={[styles.input, focus ? styles.focus : null, props.style]}
-        {...props}
-      />
-      <CustomText style={[props.error ? styles.error : null]}>
-        {props.error}
-      </CustomText>
-    </View>
-  );
+	return (
+		<View style={{ width: "100%" }}>
+			<CustomText type="medium" style={{ textTransform: "capitalize" }}>
+				{props.label}
+			</CustomText>
+			<TextInput
+				placeholder={props.placeholder}
+				secureTextEntry={props.secureTextEntry}
+				value={props.value}
+				onChangeText={props.onChangeText}
+				onFocus={() => setFocus(true)}
+				onBlur={() => setFocus(false)}
+				style={[styles.input, focus ? styles.focus : null, props.style]}
+				{...props}
+			/>
+			<CustomText style={[props.error ? styles.error : null]}>
+				{props.error}
+			</CustomText>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  input: {
-    marginTop: 4,
-    padding: 16,
-    fontSize: 16,
-    letterSpacing: 0.232836,
-    borderColor: "#B3B0AD",
-    color: "#6D6D6D",
-    borderWidth: 1,
-    backgroundColor: "#fff",
-  },
+	input: {
+		marginTop: 4,
+		padding: 16,
+		fontSize: 16,
+		letterSpacing: 0.232836,
+		borderColor: "#B3B0AD",
+		color: "#6D6D6D",
+		borderWidth: 1,
+		backgroundColor: "#fff",
+	},
 
-  focus: {
-    borderColor: colors.primary,
-    borderWidth: 1,
-  },
+	focus: {
+		borderColor: colors.primary,
+		borderWidth: 1,
+	},
 
-  error: {
-    color: "rgb(153, 0, 85)",
-    paddingBottom: 16,
-  },
+	error: {
+		color: "rgb(153, 0, 85)",
+		paddingBottom: 16,
+	},
 });
 
 Input.propTypes = {
-  label: PropTypes.string,
-  placeholder: PropTypes.string,
+	label: PropTypes.string,
+	secureTextEntry: PropTypes.bool,
+	onChangeText: PropTypes.func,
+	value: PropTypes.string,
+	placeholder: PropTypes.string,
+	error: PropTypes.any,
+	style: PropTypes.object,
 };
 
 export default Input;
