8d7b97490a8c19c70a29996df724b2c9d187d5bd
Chuks Festus <cutogu@gmail.com>
2020-11-15 17:04:18 +0100

    update

---
+++ navigation/mainNavigation.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "../screens/home";
+
+const Stack = createStackNavigator();
+
+export default function MainNavigation() {
+	return (
+		<Stack.Navigator
+			screenOptions={{
+				headerShown: false,
+			}}
+		>
+			<Stack.Screen name="login" component={Home} />
+		</Stack.Navigator>
+	);
+}
