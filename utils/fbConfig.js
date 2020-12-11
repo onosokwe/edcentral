0f30429cb3377be49114a02c7e60a9c653343d7b
Chuks Festus <cutogu@gmail.com>
2020-12-11 19:28:23 +0100

    update

--- utils/fbConfig.js
+++ utils/fbConfig.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-	apiKey: "AIzaSyA1wzM3waCF1hphdjY_3SoCFwxEIzJ0jno",
-	authDomain: "edcentral-99a06.firebaseapp.com",
-	databaseURL: "https://edcentral-99a06.firebaseio.com",
-	projectId: "edcentral-99a06",
-	storageBucket: "edcentral-99a06.appspot.com",
-	messagingSenderId: "702152286715",
-	appId: "1:702152286715:web:2f4b81f7b2fa4217862b5e",
-	measurementId: "G-MRNY7VFNMN",
+  apiKey: "AIzaSyA1wzM3waCF1hphdjY_3SoCFwxEIzJ0jno",
+  authDomain: "edcentral-99a06.firebaseapp.com",
+  databaseURL: "https://edcentral-99a06.firebaseio.com",
+  projectId: "edcentral-99a06",
+  storageBucket: "edcentral-99a06.appspot.com",
+  messagingSenderId: "702152286715",
+  appId: "1:702152286715:web:2f4b81f7b2fa4217862b5e",
+  measurementId: "G-MRNY7VFNMN",
 };
