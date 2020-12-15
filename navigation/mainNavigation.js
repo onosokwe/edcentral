cdd2c78993f5cfa208b7e14243a4cca21da64020
Chuks Festus <cutogu@gmail.com>
2020-12-15 03:16:42 +0100

    added inner screens

--- navigation/mainNavigation.js
+++ navigation/mainNavigation.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useSelector } from "react-redux";
 
 import Home from "../screens/homeScreen";
 import ConfirmEmailScreen from "../screens/ConfirmEmailScreen";
+import Schools from "../screens/SchoolsScreen";
+import ScholarShip from "../screens/ScholarShipScreen";
+import Profile from "../screens/ProfileScreen";
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 const ConfirmStack = createStackNavigator();
 
 export default function MainNavigation() {
@@ -19,13 +23,12 @@ export default function MainNavigation() {
           <ConfirmStack.Screen name="confirm" component={ConfirmEmailScreen} />
         </ConfirmStack.Navigator>
       ) : (
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="home" component={Home} />
-        </Stack.Navigator>
+        <Tab.Navigator>
+          <Tab.Screen name="home" component={Home} />
+          <Tab.Screen name="profile" component={Profile} />
+          <Tab.Screen name="school" component={Schools} />
+          <Tab.Screen name="scholarships" component={ScholarShip} />
+        </Tab.Navigator>
       )}
     </React.Fragment>
   );
---
+++ screens/ProfileScreen.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import CustomText from "../atoms/text";
+
+export default function Profile() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <CustomText>Profile</CustomText>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+});
