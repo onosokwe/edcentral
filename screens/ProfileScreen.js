3dcd91b5eb627dd74367900ae594859d9063acc6
Chuks Festus <cutogu@gmail.com>
2020-12-24 18:24:06 +0100

    profile screen update

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Image, StyleSheet, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { useSelector } from "react-redux";
 
 import CustomText from "../atoms/text";
@@ -10,52 +9,48 @@ import ProfileSvg from "../components/svg/profile";
 export default function Profile() {
   const { user } = useSelector((state) => state.user);
   return (
-    <SafeAreaView
-      style={{ flex: 1, backgroundColor: "hsla(218,100%,49%,0.3)" }}
-    >
-      <View style={styles.container}>
-        <View style={styles.profileSum}>
-          <View style={styles.imageContainer}>
-            <ProfileSvg width={53} height={67} />
-          </View>
-          <CustomText style={styles.name} type="semiBold">
-            {user.name}
-          </CustomText>
-          <CustomText style={styles.email}>{user.email}</CustomText>
-          <CustomText style={styles.email}>{user.number}</CustomText>
+    <View style={styles.container}>
+      <View style={styles.profileSum}>
+        <View style={styles.imageContainer}>
+          <ProfileSvg width={53} height={67} />
         </View>
-        <View style={styles.body}>
-          <Image
-            style={{ width: 279.07, height: 202.29 }}
-            source={require("../assets/images/profile-empty.png")}
-          />
-          <CustomText
-            style={{
-              textAlign: "center",
-              maxWidth: 287,
-              paddingTop: 35,
-              color: colors.LightBlack,
-            }}
-          >
-            you haven’t applied for any scholarship yet. go to scholarships
-            section to apply
-          </CustomText>
-        </View>
+        <CustomText style={styles.name} type="semiBold">
+          {user.name}
+        </CustomText>
+        <CustomText style={styles.email}>{user.email}</CustomText>
+        <CustomText style={styles.email}>{user.number}</CustomText>
       </View>
-    </SafeAreaView>
+      <View style={styles.body}>
+        <Image
+          style={{ width: 279.07, height: 202.29 }}
+          source={require("../assets/images/profile-empty.png")}
+        />
+        <CustomText
+          style={{
+            textAlign: "center",
+            maxWidth: 287,
+            paddingTop: 35,
+            color: colors.LightBlack,
+          }}
+        >
+          you haven’t applied for any scholarship yet. go to scholarships
+          section to apply
+        </CustomText>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    resizeMode: "cover",
   },
   profileSum: {
     height: "50%",
     width: "100%",
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "hsla(218,100%,49%,0.3)",
   },
   imageContainer: {
     height: 130,
@@ -77,7 +72,7 @@ const styles = StyleSheet.create({
   },
   body: {
     backgroundColor: colors.notWhite,
-    height: "60%",
+    height: "50%",
     justifyContent: "center",
     alignItems: "center",
   },
