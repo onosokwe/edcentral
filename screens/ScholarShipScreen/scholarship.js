d3bf8207158bbfb7d1077c6c4a7a248d37f9443d
Chuks Festus <cutogu@gmail.com>
2021-01-02 10:06:10 +0100

    des

--- screens/ScholarShipScreen/scholarship.js
+++ screens/ScholarShipScreen/scholarship.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Pressable, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { CommonActions } from "@react-navigation/native";
 import PropTypes from "prop-types";
 
 import colors from "../../atoms/colors";
 import CustomText from "../../atoms/text";
 import { InnerCard } from "../../components/scholarshipCard";
 import BackButtonSvg from "../../components/svg/backButton";
+import BookmarkSvg from "../../components/svg/bookmark";
 
 export default function Scholarship(props) {
   return (
@@ -18,15 +18,40 @@ export default function Scholarship(props) {
             width: "90%",
           }}
         >
-          <View style={{ paddingBottom: 40 }}>
-            <Pressable
-              onPress={() => props.navigation.dispatch(CommonActions.goBack())}
-            >
+          <View style={styles.top}>
+            <Pressable onPress={() => props.navigation.goBack()}>
               <BackButtonSvg />
+            </Pressable>
+            <Pressable
+              style={styles.apply}
+              onPress={() => console.log("applied")}
+            >
+              <CustomText style={{ color: colors.notWhite, fontSize: 16 }}>
+                Apply
+              </CustomText>
             </Pressable>
           </View>
           <View style={{ flexDirection: "row" }}>
             <InnerCard size={90} />
+          </View>
+          <View style={styles.des}>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                paddingBottom: 16,
+                alignItems: "center",
+              }}
+            >
+              <CustomText type="medium">Description</CustomText>
+              <BookmarkSvg />
+            </View>
+            <CustomText style={{ color: colors.LightBlack }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae
+              varius commodo feugiat vitae eu eu. Cras ut tempor sit enim. Sed
+              nisl elementum lectus at condimentum. Enim mattis ornare bibendum
+              blandit sed pretium pharetra convallis sit.
+            </CustomText>
           </View>
         </View>
       </View>
@@ -41,6 +66,20 @@ const styles = StyleSheet.create({
     alignItems: "center",
     width: "100%",
     paddingTop: 30,
+  },
+  top: {
+    paddingBottom: 40,
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  apply: {
+    backgroundColor: colors.primary,
+    padding: 10,
+    borderRadius: 6,
+  },
+  des: {
+    paddingTop: 10,
   },
 });
 
